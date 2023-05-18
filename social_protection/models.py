c62cb26a51518cc2e80a906c6d9faf855a401eb7
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 12:38:04 +0200

    CM-25: create BenefitPlan model

--- social_protection/models.py
+++ social_protection/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from core import models as core_models
+from policyholder.models import PolicyHolder
 
-# Create your models here.
+
+class BenefitPlan(core_models.HistoryBusinessModel):
+    code = models.CharField(db_column='Code', max_length=50, null=False)
+    name = models.CharField(db_column='NameBF', max_length=255, null=False)
+    date_from = models.DateTimeField(db_column="DateFrom")
+    date_to = models.DateTimeField(db_column="DateTo")
+    max_beneficiaries = models.SmallIntegerField(db_column="MaxNoBeneficiaries")
+    ceiling_per_beneficiary = models.DecimalField(db_column="BeneficiaryCeiling",
+                                                  max_digits=18,
+                                                  decimal_places=2,
+                                                  blank=True,
+                                                  null=True,
+                                                  )
+    organization = models.ForeignKey(PolicyHolder, models.DO_NOTHING, db_column='Organization', blank=True, null=True)
+    schema = models.TextField(db_column="Schema", null=True, blank=True)
+
+    class Meta:
+        managed = True
+        db_table = 'tblBenefitPlan'
