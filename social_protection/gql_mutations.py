33f0283bc64d077cc769f89ad37aa5ce1109fe59
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 16:50:27 +0200

    CM-25: route gql mutations through service

--- social_protection/gql_mutations.py
+++ social_protection/gql_mutations.py
@@ -1,12 +1,14 @@
 import graphene as graphene
 from django.contrib.auth.models import AnonymousUser
 from pydantic.error_wrappers import ValidationError
+from django.db import transaction
 
 from core.gql.gql_mutations.base_mutation import BaseHistoryModelCreateMutationMixin, BaseMutation, \
     BaseHistoryModelUpdateMutationMixin, BaseHistoryModelDeleteMutationMixin
 from core.schema import OpenIMISMutation
 from social_protection.apps import SocialProtectionConfig
 from social_protection.models import BenefitPlan
+from social_protection.services import BenefitPlanService
 
 
 class CreateBenefitPlanInputType(OpenIMISMutation.Input):
@@ -16,7 +18,7 @@ class CreateBenefitPlanInputType(OpenIMISMutation.Input):
     date_to = graphene.Date(required=True)
     max_beneficiaries = graphene.Int(default_value=0)
     ceiling_per_beneficiary = graphene.Decimal(max_digits=18, decimal_places=2, required=False)
-    organization_id = graphene.Int(required=False)
+    organization_id = graphene.UUID(required=False)
     schema = graphene.String()
 
     date_valid_from = graphene.Date(required=False)
@@ -39,6 +41,16 @@ class CreateBenefitPlanMutation(BaseHistoryModelCreateMutationMixin, BaseMutatio
                 SocialProtectionConfig.gql_benefit_plan_create_perms):
             raise ValidationError("mutation.authentication_required")
 
+    @classmethod
+    def _mutate(cls, user, **data):
+        if "client_mutation_id" in data:
+            data.pop('client_mutation_id')
+        if "client_mutation_label" in data:
+            data.pop('client_mutation_label')
+
+        service = BenefitPlanService(user)
+        service.create(data)
+
     class Input(CreateBenefitPlanInputType):
         pass
 
@@ -53,6 +65,18 @@ class UpdateBenefitPlanMutation(BaseHistoryModelUpdateMutationMixin, BaseMutatio
         if type(user) is AnonymousUser or not user.has_perms(
                 SocialProtectionConfig.gql_benefit_plan_update_perms):
             raise ValidationError("mutation.authentication_required")
+
+    @classmethod
+    def _mutate(cls, user, **data):
+        if "date_valid_to" not in data:
+            data['date_valid_to'] = None
+        if "client_mutation_id" in data:
+            data.pop('client_mutation_id')
+        if "client_mutation_label" in data:
+            data.pop('client_mutation_label')
+
+        service = BenefitPlanService(user)
+        service.update(data)
 
     class Input(UpdateBenefitPlanInputType):
         pass
@@ -69,5 +93,20 @@ class DeleteBenefitPlanMutation(BaseHistoryModelDeleteMutationMixin, BaseMutatio
                 SocialProtectionConfig.gql_benefit_plan_delete_perms):
             raise ValidationError("mutation.authentication_required")
 
+    @classmethod
+    def _mutate(cls, user, **data):
+        if "client_mutation_id" in data:
+            data.pop('client_mutation_id')
+        if "client_mutation_label" in data:
+            data.pop('client_mutation_label')
+
+        service = BenefitPlanService(user)
+
+        ids = data.get('ids')
+        if ids:
+            with transaction.atomic():
+                for id in ids:
+                    service.delete({'id': id})
+
     class Input(OpenIMISMutation.Input):
-        uuids = graphene.List(graphene.UUID)
+        ids = graphene.List(graphene.UUID)
