8e423c66c11bfcf68f6e930801e471bb20b13491
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 12:58:22 +0200

    CM-25: add gql_mutations

---
+++ social_protection/gql_mutations.py
@@ -0,0 +1,68 @@
+import graphene as graphene
+from django.contrib.auth.models import AnonymousUser
+from pydantic.error_wrappers import ValidationError
+
+from core.gql.gql_mutations.base_mutation import BaseHistoryModelCreateMutationMixin, BaseMutation, \
+    BaseHistoryModelUpdateMutationMixin, BaseHistoryModelDeleteMutationMixin
+from core.schema import OpenIMISMutation
+from social_protection.apps import SocialProtectionConfig
+from social_protection.models import BenefitPlan
+
+
+class CreateBenefitPlanInputType(OpenIMISMutation.Input):
+    name = graphene.String(required=True, max_length=255)
+    date_from = graphene.Date(required=True)
+    date_to = graphene.Date(required=True)
+    max_beneficiaries = graphene.Int(default_value=0)
+    ceiling_per_beneficiary = graphene.Decimal(max_digits=18, decimal_places=2, required=False)
+    organization_id = graphene.Int(required=False)
+    schema = graphene.String()
+
+
+class UpdateBenefitPlanInputType(CreateBenefitPlanInputType):
+    id = graphene.UUID(required=True)
+
+
+class CreateBenefitPlanMutation(BaseHistoryModelCreateMutationMixin, BaseMutation):
+    _mutation_class = "CreateBenefitPlanMutation"
+    _mutation_module = "social_protection"
+    _model = BenefitPlan
+
+    @classmethod
+    def _validate_mutation(cls, user, **data):
+        if type(user) is AnonymousUser or not user.has_perms(
+                SocialProtectionConfig.gql_benefit_plan_create_perms):
+            raise ValidationError("mutation.authentication_required")
+
+    class Input(CreateBenefitPlanInputType):
+        pass
+
+
+class UpdateBenefitPlanMutation(BaseHistoryModelUpdateMutationMixin, BaseMutation):
+    _mutation_class = "UpdateBenefitPlanMutation"
+    _mutation_module = "social_protection"
+    _model = BenefitPlan
+
+    @classmethod
+    def _validate_mutation(cls, user, **data):
+        if type(user) is AnonymousUser or not user.has_perms(
+                SocialProtectionConfig.gql_benefit_plan_update_perms):
+            raise ValidationError("mutation.authentication_required")
+
+    class Input(UpdateBenefitPlanInputType):
+        pass
+
+
+class DeleteBenefitPlanMutation(BaseHistoryModelDeleteMutationMixin, BaseMutation):
+    _mutation_class = "DeleteBenefitPlanMutation"
+    _mutation_module = "social_protection"
+    _model = BenefitPlan
+
+    @classmethod
+    def _validate_mutation(cls, user, **data):
+        if type(user) is AnonymousUser or not user.id or not user.has_perms(
+                SocialProtectionConfig.gql_benefit_plan_delete_perms):
+            raise ValidationError("mutation.authentication_required")
+
+    class Input(OpenIMISMutation.Input):
+        uuids = graphene.List(graphene.UUID)
