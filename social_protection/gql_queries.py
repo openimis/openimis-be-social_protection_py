b1332941cbbf9f380af7ff7299a77418d9042c27
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 13:26:20 +0200

    CM-25: create BenefitPlanGQLType

---
+++ social_protection/gql_queries.py
@@ -0,0 +1,30 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from core import prefix_filterset, ExtendedConnection
+from policyholder.gql import PolicyHolderGQLType
+from social_protection.models import BenefitPlan
+
+
+class BenefitPlanGQLType(DjangoObjectType):
+    uuid = graphene.String(source='uuid')
+
+    class Meta:
+        model = BenefitPlan
+        interfaces = (graphene.relay.Node,)
+        filtered_fields = {
+            "id": ["exact"],
+            "code": ["exact", "iexact", "startswith", "istartswith", "contains", "icontains"],
+            "name": ["exact", "iexact", "startswith", "istartswith", "contains", "icontains"],
+            "date_from": ["exact", "lt", "lte", "gt", "gte"],
+            "date_to": ["exact", "lt", "lte", "gt", "gte"],
+            "max_beneficiaries": ["exact", "lt", "lte", "gt", "gte"],
+            "schema": ["exact", "iexact", "startswith", "istartswith", "contains", "icontains"],
+            **prefix_filterset("policyholder__", PolicyHolderGQLType._meta.filter_fields),
+
+            "date_created": ["exact", "lt", "lte", "gt", "gte"],
+            "date_updated": ["exact", "lt", "lte", "gt", "gte"],
+            "is_deleted": ["exact"],
+            "version": ["exact"],
+        }
+        connection_class = ExtendedConnection
