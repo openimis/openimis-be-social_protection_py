0ef3f073cb8be1639fae51722a6d18792303e78e
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 13:59:07 +0200

    CM-25: create initial test

--- social_protection/tests.py
+++ social_protection/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BenefitPlanTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_example_module_loaded_correctly(self):
+        self.assertTrue(True)
