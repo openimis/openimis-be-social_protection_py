830f5297b33e5ab17ee382b69bf1eb18c234b293
jdolkowski <jdolkowski@soldevelo.com>
2023-05-18 12:14:34 +0200

    CM-25: add social_protection app config

--- social_protection/apps.py
+++ social_protection/apps.py
@@ -1,6 +1,33 @@
 from django.apps import AppConfig
 
+DEFAULT_CONFIG = {
+    "gql_benefit_plan_search_perms": ["160001"],
+    "gql_benefit_plan_create_perms": ["160002"],
+    "gql_benefit_plan_update_perms": ["160003"],
+    "gql_benefit_plan_delete_perms": ["160004"],
+}
+
 
 class SocialProtectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'social_protection'
+
+    gql_benefit_plan_search_perms = None
+    gql_benefit_plan_create_perms = None
+    gql_benefit_plan_update_perms = None
+    gql_benefit_plan_delete_perms = None
+
+    def ready(self):
+        from core.models import ModuleConfiguration
+
+        cfg = ModuleConfiguration.get_or_default(self.name, DEFAULT_CONFIG)
+        self.__load_config(cfg)
+
+    @classmethod
+    def __load_config(cls, cfg):
+        """
+        Load all config fields that match current AppConfig class fields, all custom fields have to be loaded separately
+        """
+        for field in cfg:
+            if hasattr(SocialProtectionConfig, field):
+                setattr(SocialProtectionConfig, field, cfg[field])
