9af73724fd9336069b022226fd462a009e62fcc5
jdolkowski <jdolkowski@soldevelo.com>
2023-05-19 14:15:54 +0200

    CM-25: change filed naming convetion to autogenerated in BenefitPlan

--- social_protection/models.py
+++ social_protection/models.py
@@ -4,14 +4,13 @@ from policyholder.models import PolicyHolder
 
 
 class BenefitPlan(core_models.HistoryBusinessModel):
-    code = models.CharField(db_column='Code', max_length=8, null=False)
-    name = models.CharField(db_column='NameBF', max_length=255, null=False)
-    max_beneficiaries = models.SmallIntegerField(db_column="MaxNoBeneficiaries")
-    ceiling_per_beneficiary = models.DecimalField(db_column="BeneficiaryCeiling",
-                                                  max_digits=18,
+    code = models.CharField(max_length=8, null=False)
+    name = models.CharField(max_length=255, null=False)
+    max_beneficiaries = models.SmallIntegerField()
+    ceiling_per_beneficiary = models.DecimalField(max_digits=18,
                                                   decimal_places=2,
                                                   blank=True,
                                                   null=True,
                                                   )
-    holder = models.ForeignKey(PolicyHolder, models.DO_NOTHING, db_column='Holder', blank=True, null=True)
-    schema = models.TextField(db_column="Schema", null=True, blank=True)
+    holder = models.ForeignKey(PolicyHolder, models.DO_NOTHING, blank=True, null=True)
+    beneficiary_data_schema = models.TextField(null=True, blank=True)
