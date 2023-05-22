9a772038dbc10e843a7924ef58191956606fa69e
sniedzielski <sniedzielski@soldevelo.com>
2023-05-22 17:42:43 +0200

    CM-27: fixed tests naming and creation of test data

--- social_protection/tests/beneficiary_service_test.py
+++ social_protection/tests/beneficiary_service_test.py
@@ -28,28 +28,35 @@ class BeneficiaryServiceTest(TestCase):
         cls.query_all = Beneficiary.objects.filter(is_deleted=False)
         cls.benefit_plan = cls.__create_benefit_plan()
         cls.individual = cls.__create_individual()
+        cls.payload = {
+            **service_beneficiary_add_payload,
+            "individual_id": cls.individual.id,
+            "benefit_plan_id": cls.benefit_plan.id
+        }
 
-    def test_add_benefitiary(self):
-        result = self.service.create(service_beneficiary_add_payload)
+    def test_add_beneficiary(self):
+        result = self.service.create(self.payload)
         self.assertTrue(result.get('success', False), result.get('detail', "No details provided"))
         uuid = result.get('data', {}).get('uuid', None)
         query = self.query_all.filter(uuid=uuid)
         self.assertEqual(query.count(), 1)
 
-    def test_update_benefit_plan(self):
-        result = self.service.create(service_beneficiary_add_payload)
+    def test_update_beneficiary(self):
+        result = self.service.create(self.payload)
         self.assertTrue(result.get('success', False), result.get('detail', "No details provided"))
         uuid = result.get('data', {}).get('uuid')
         update_payload = copy.deepcopy(service_beneficiary_update_payload)
         update_payload['id'] = uuid
+        update_payload['individual_id'] = self.individual.id
+        update_payload['benefit_plan_id'] = self.benefit_plan.id
         result = self.service.update(service_beneficiary_update_payload)
         self.assertTrue(result.get('success', False), result.get('detail', "No details provided"))
         query = self.query_all.filter(uuid=uuid)
         self.assertEqual(query.count(), 1)
         self.assertEqual(query.first().status, update_payload.get('status'))
 
-    def test_delete_benefit_plan(self):
-        result = self.service.create(service_beneficiary_add_payload)
+    def test_delete_beneficiary(self):
+        result = self.service.create(self.payload)
         self.assertTrue(result.get('success', False), result.get('detail', "No details provided"))
         uuid = result.get('data', {}).get('uuid')
         delete_payload = {'id': uuid}
