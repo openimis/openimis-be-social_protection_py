8771deef9f057cd5039ce576e5d0b731d797f2a8
sniedzielski <sniedzielski@soldevelo.com>
2023-06-02 11:56:55 +0200

    CM-69: removed redundant docstring, fixed docstring

--- social_protection/custom_filters.py
+++ social_protection/custom_filters.py
@@ -16,28 +16,28 @@ class BenefitPlanCustomFilterWizard(CustomFilterWizardInterface):
 
     def get_type_of_object(self) -> str:
         """
-            Get the type of object for which we want to define a specific way of building filters.
+        Get the type of object for which we want to define a specific way of building filters.
 
-            Returns:
-                str: The type of the object.
+        :return: The type of the object.
+        :rtype: str
         """
         return self.OBJECT_CLASS.__name__
 
     def load_definition(self, tuple_type: type, **kwargs) -> List[namedtuple]:
         """
-            Load the definition of how to create filters.
+        Load the definition of how to create filters.
 
-            This method retrieves the definition of how to create filters and returns it as a list of named tuples.
-            Each named tuple is built with the provided `tuple_type` and has the fields `field`, `filter`, and `value`.
+        This method retrieves the definition of how to create filters and returns it as a list of named tuples.
+        Each named tuple is built with the provided `tuple_type` and has the fields `field`, `filter`, and `value`.
 
-            Example named tuple: <Type>(field=<str>, filter=<str>, value=<str>)
-            Example usage: BenefitPlan(field='income', filter='lt, gte, icontains, exact', type='')
+        Example named tuple: <Type>(field=<str>, filter=<str>, value=<str>)
+        Example usage: BenefitPlan(field='income', filter='lt, gte, icontains, exact', value='')
 
-            Args:
-                tuple_type (type): The type of the named tuple.
+        :param tuple_type: The type of the named tuple.
+        :type tuple_type: type
 
-            Returns:
-                List[namedtuple]: A list of named tuples representing the definition of how to create filters.
+        :return: A list of named tuples representing the definition of how to create filters.
+        :rtype: List[namedtuple]
         """
         logger.info('starting loading definition of custom filter')
         list_of_tuple_with_definitions = []
@@ -59,20 +59,6 @@ class BenefitPlanCustomFilterWizard(CustomFilterWizardInterface):
         tuple_type: type,
         list_of_tuple_with_definitions: List[namedtuple]
     ) -> None:
-        """
-            Process the schema for the obtained BenefitPlan instance and load the schema
-            definition into the desired output.
-
-            Args:
-                benefit_plan (BenefitPlan): The instance of the chosen BenefitPlan where the schema is
-                 presented for a specific program.
-                tuple_type (type): The type of the named tuple.
-                list_of_tuple_with_definitions (List[namedtuple]): A list of named tuples representing
-                 the definition of how to create filters.
-
-            Returns:
-                None: This is a void method.
-        """
         schema = benefit_plan.beneficiary_data_schema
         if schema:
             if 'properties' in schema:
