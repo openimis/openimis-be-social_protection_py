c45458671c4403617470c92e8bf15a2b7474b55a
Kamil Malinowski <kmalinowski@soldevelo.com>
2023-07-10 15:55:55 +0200

    CM-188 Adjusted backend for social protection

--- social_protection/apps.py
+++ social_protection/apps.py
@@ -31,8 +31,6 @@ class SocialProtectionConfig(AppConfig):
     def ready(self):
         from core.models import ModuleConfiguration
 
-        print("Hello automatic update")
-
         cfg = ModuleConfiguration.get_or_default(self.name, DEFAULT_CONFIG)
         self.__load_config(cfg)
 
