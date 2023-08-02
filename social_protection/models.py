0e9eb5248b2a448a79005960101543959400092b
Jan <jdolkowski@soldevelo.com>
2023-08-02 09:08:22 +0200

    CM-197: add perms for schema CRUD (#28)

--- social_protection/models.py
+++ social_protection/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from django.db import models
 from django.utils.translation import gettext as _
 from pydantic.error_wrappers import ValidationError
