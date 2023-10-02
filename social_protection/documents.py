be1f1c41daaa0644be9ee5b773460e1df4c88381
sniedzielski <sniedzielski@soldevelo.com>
2023-10-02 09:45:18 +0200

    CM-269: removed comments

--- social_protection/documents.py
+++ social_protection/documents.py
@@ -23,7 +23,7 @@ if 'opensearch_reports' in apps.app_configs:
         )
 
         class Index:
-            name = 'beneficiary'  # Name of the Opensearch index
+            name = 'beneficiary'
             settings = {
                 'number_of_shards': 1,
                 'number_of_replicas': 0
@@ -39,11 +39,7 @@ if 'opensearch_reports' in apps.app_configs:
             queryset_pagination = 5000
 
         def get_instances_from_related(self, related_instance):
-            """Define how to retrieve the Car instance(s) from the related model.
-            The related_models option should be used with caution because it can lead to updating many items in the index.
-            """
             if isinstance(related_instance, BenefitPlan):
                 return related_instance.beneficiary_set.all()
             elif isinstance(related_instance, Individual):
                 return related_instance.beneficiary_set.all()
-
