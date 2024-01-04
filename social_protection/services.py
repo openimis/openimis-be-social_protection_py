e9889b25e1bc7cd3528b27578b4f492a9a3bf148
sniedzielski <sniedzielski@soldevelo.com>
2024-01-04 20:05:08 +0100

    validation workflow2

--- social_protection/services.py
+++ social_protection/services.py
@@ -217,6 +217,9 @@ class BeneficiaryImportService:
                           upload: IndividualDataSourceUpload,
                           benefit_plan: BenefitPlan,
                           import_file: InMemoryUploadedFile):
+        print('xxxxx')
+        print(import_file)
+        print('xxxxx')
         workflow.run({
             # Core user UUID required
             'user_uuid': str(User.objects.get(username=self.user.login_name).id),
