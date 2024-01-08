1b485bec21849984784eff6f62279accf9367a8d
sniedzielski <sniedzielski@soldevelo.com>
2024-01-08 13:47:31 +0100

    validation workflow16

--- social_protection/urls.py
+++ social_protection/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import import_beneficiaries, validate_import_beneficiaries
+from .views import (
+    import_beneficiaries,
+    validate_import_beneficiaries,
+    create_task_with_importing_valid_items
+)
 
 urlpatterns = [
     path('import_beneficiaries/', import_beneficiaries),
     path('validate_import_beneficiaries/', validate_import_beneficiaries),
+    path('create_task_with_importing_valid_items/', create_task_with_importing_valid_items),
 ]
