dba490e79c79d0b1c41cefc3429e248227b561fc
Jan <j.dolkowski@soldevelo.com>
2024-01-30 01:34:45 +0100

    Revert "test-branch"
    This reverts commit 5eafd83ff7acaa679fab3a6c614c364185b69a40.

--- social_protection/gql_mutations.py
+++ social_protection/gql_mutations.py
@@ -251,7 +251,7 @@ class UpdateBeneficiaryMutation(BaseHistoryModelUpdateMutationMixin, BaseMutatio
             data.pop('client_mutation_label')
 
         service = BeneficiaryService(user)
-        if False:
+        if SocialProtectionConfig.gql_check_beneficiary_crud:
             res = service.create_update_task(data)
         else:
             res = service.update(data)
